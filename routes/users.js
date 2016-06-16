44943ecac0d839187d7c9151603fd885571d07e1
Dinh Le <dinh.v.le@gmail.com>
2016-06-16 13:38:33 -0500

    getting CRUD done

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,41 @@
 var express = require('express');
 var router = express.Router();
+var knex = require("../db/knex");
+var User = require("../models/user");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+require('locus');
+
+// Index, fetch all users
+router.get('/', (req,res) => {
+  User.forge().fetchAll().then((collection) => {
+
+  })
+})
+
+
+// Create a new user
+
+router.get('/new', (req,res) => {
+  res.render("users/new");
+})
+
+// router.post('/', (req,res) => {
+//   User.forge(req.body.user).save().then((user) => {
+//     res.redirect('/users');
+//   })
+// })
+router.post('/', function(req, res) {
+  Author.forge(req.body.user).save().then(function(author) {
+    res.redirect('/users');
+  });
 });
+// Read, fetch a single user by id
+router.get('/:id', (req,res) => {
+  User.forge({id: req.params.id}).fetch().then((user) => {
+    res.render('users/show', {user: user.toJSON()})
+  })
+})
+
+
 
 module.exports = router;
